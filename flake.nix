aae4f19789401fb90937ac203145135f89400b85
Mark Joshwel <mark@joshwel.co>
2025-09-16 14:54:33 +0800

    nix, tests: `nix flake check` now works

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "develop or deploy Lotte Music Friend";
+  description = "develop or build/package tomlantic";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
@@ -27,7 +27,7 @@
 
   outputs =
     {
-      self,
+      # self,
       nixpkgs,
       uv2nix,
       pyproject-nix,
@@ -40,40 +40,21 @@
         inherit (nixpkgs) lib;
         pkgs = nixpkgs.legacyPackages.${system};
 
-        # Load a uv workspace from a workspace root.
-        # Uv2nix treats all uv projects as workspace projects.
+        # create package overlay from workspace/project
         workspace = uv2nix.lib.workspace.loadWorkspace { workspaceRoot = ./.; };
-
-        # Create package overlay from workspace.
         overlay = workspace.mkPyprojectOverlay {
-          # Prefer prebuilt binary wheels as a package source.
-          # Sdists are less likely to "just work" because of the metadata missing from uv.lock.
-          # Binary wheels are more likely to, but may still require overrides for library dependencies.
-          sourcePreference = "wheel"; # or sourcePreference = "sdist";
-          # Optionally customise PEP 508 environment
-          # environ = {
-          #   platform_release = "5.10.65";
-          # };
+          sourcePreference = "wheel";
         };
 
-        # Extend generated overlay with build fixups
-        #
-        # Uv2nix can only work with what it has, and uv.lock is missing essential metadata to perform some builds.
-        # This is an additional overlay implementing build fixups.
-        # See:
-        # - https://pyproject-nix.github.io/uv2nix/FAQ.html
+        # extend generated overlay with build fixups
         pyprojectOverrides = _final: _prev: {
-          # Implement build fixups here.
-          # Note that uv2nix is _not_ using Nixpkgs buildPythonPackage.
-          # It's using https://pyproject-nix.github.io/pyproject.nix/build.html
+          # https://pyproject-nix.github.io/uv2nix/overriding/index.html
         };
 
-        # Use Python 3.12 from nixpkgs
+        # construct package set
         python = pkgs.python313;
-
-        # Construct package set
         pythonSet =
-          # Use base package set from pyproject.nix builders
+          # use base package set from pyproject.nix builders
           (pkgs.callPackage pyproject-nix.build.packages {
             inherit python;
           }).overrideScope
@@ -86,15 +67,13 @@
             );
       in
       {
-        # Package a virtual environment as our main application.
-        #
-        # Enable no optional dependencies for production build.
+        # package a virtual environment as our main application with no optional dependencies
         packages.default = pythonSet.mkVirtualEnv "tomlantic-env" workspace.deps.default;
 
         # nix develop
         devShells = {
-          # It is of course perfectly OK to keep using an impure virtualenv workflow and only use uv2nix to build packages.
-          # This devShell simply adds Python and undoes the dependency leakage done by Nixpkgs Python infrastructure.
+          # it is of course perfectly OK to keep using an impure virtualenv workflow and only use uv2nix to build packages
+          # this devShell simply adds Python and undoes the dependency leakage done by Nixpkgs Python infrastructure
           impure = pkgs.mkShell {
             packages = [
               python
@@ -102,14 +81,11 @@
             ];
             env =
               {
-                # Prevent uv from managing Python downloads
+                # prevent uv from managing python downloads
                 UV_PYTHON_DOWNLOADS = "never";
-                # Force uv to use nixpkgs Python interpreter
                 UV_PYTHON = python.interpreter;
               }
               // lib.optionalAttrs pkgs.stdenv.isLinux {
-                # Python libraries often load native shared objects using dlopen(3).
-                # Setting LD_LIBRARY_PATH makes the dynamic library loader aware of libraries without using RPATH for lookup.
                 LD_LIBRARY_PATH = lib.makeLibraryPath pkgs.pythonManylinuxPackages.manylinux1;
               };
             shellHook = ''
@@ -117,31 +93,21 @@
             '';
           };
 
-          # This devShell uses uv2nix to construct a virtual environment purely from Nix, using the same dependency specification as the application.
-          # The notable difference is that we also apply another overlay here enabling editable mode ( https://setuptools.pypa.io/en/latest/userguide/development_mode.html ).
-          #
-          # This means that any changes done to your local files do not require a rebuild.
-          #
-          # Note: Editable package support is still unstable and subject to change.
           default =
             let
-              # Create an overlay enabling editable mode for all local dependencies.
               editableOverlay = workspace.mkEditablePyprojectOverlay {
-                # Use environment variable
                 root = "$REPO_ROOT";
-                # Optional: Only enable editable for these packages
-                # members = [ "hello-world" ];
               };
 
-              # Override previous set with our overrideable overlay.
+              # override previous set with our overrideable overlay.
               editablePythonSet = pythonSet.overrideScope (
                 lib.composeManyExtensions [
                   editableOverlay
 
-                  # Apply fixups for building an editable package of your workspace packages
+                  # apply fixups for building an editable package of your workspace packages
                   (final: prev: {
                     hello-world = prev.hello-world.overrideAttrs (old: {
-                      # It's a good idea to filter the sources going into an editable build
+                      # it's a good idea to filter the sources going into an editable build
                       # so the editable package doesn't have to be rebuilt on every change.
                       src = lib.fileset.toSource {
                         root = old.src;
@@ -152,12 +118,12 @@
                         ];
                       };
 
-                      # Hatchling (our build system) has a dependency on the `editables` package when building editables.
+                      # hatchling (our build system) has a dependency on the `editables` package when building editables
                       #
-                      # In normal Python flows this dependency is dynamically handled, and doesn't need to be explicitly declared.
-                      # This behaviour is documented in PEP-660.
+                      # in normal Python flows, this dependency is dynamically handled, and doesn't need to be explicitly declared.
+                      # this behaviour is documented in PEP-660
                       #
-                      # With Nix the dependency needs to be explicitly declared.
+                      # with Nix the dependency needs to be explicitly declared
                       nativeBuildInputs =
                         old.nativeBuildInputs
                         ++ final.resolveBuildSystem {
@@ -169,9 +135,7 @@
                 ]
               );
 
-              # Build virtual environment, with local packages being editable.
-              #
-              # Enable all optional dependencies for development.
+              # build virtual environment, with local packages being editable with all optional dependencies
               virtualenv = editablePythonSet.mkVirtualEnv "tomlantic-dev-env" workspace.deps.all;
 
             in
@@ -182,24 +146,33 @@
               ];
 
               env = {
-                # Don't create venv using uv
-                UV_NO_SYNC = "1";
-
-                # Force uv to use nixpkgs Python interpreter
                 UV_PYTHON = python.interpreter;
-
-                # Prevent uv from downloading managed Python's
+                # uv: don't create venvs
+                UV_NO_SYNC = "1";
+                # uv: don't manage/download python
                 UV_PYTHON_DOWNLOADS = "never";
               };
 
               shellHook = ''
-                # Undo dependency propagation by nixpkgs.
                 unset PYTHONPATH
-
-                # Get repository root using git. This is expanded at runtime by the editable `.pth` machinery.
                 export REPO_ROOT=$(git rev-parse --show-toplevel)
               '';
             };
+        };
+        
+        # checks/tests
+        
+        checks = let
+           test-virtualenv = pythonSet.mkVirtualEnv "tomlantic-testing-env" workspace.deps.all;
+        in  {
+          tests = pkgs.runCommandLocal "tomlantic-check-tests" {
+            src = ./.;
+            buildInputs = [ test-virtualenv pkgs.python313 ];
+          } ''
+              cd ${./.}
+              python tests/test.py
+              mkdir -p $out
+            '';
         };
       }
     );
--- tests/test.py
+++ tests/test.py
@@ -1,22 +1,98 @@
+from os import getenv
 from pathlib import Path
+from shutil import which
 from subprocess import CompletedProcess, run
-from sys import stderr
+from sys import argv, executable, stderr
 from textwrap import indent
 
 
-def main():
+def resolve_binaries() -> dict[str, list[str]]:
+    versions_and_binaries: dict[str, list[str]] = {
+        "39": [],
+        "310": [],
+        "311": [],
+        "312": [],
+        "313": [],
+        "314": [],
+    }
+
+    # test for uv, and/or signs of being run from the nix flake, or being in a nix environment
+    if any(
+        [
+            getenv("name", default="").startswith("nix"),
+            getenv("NIX_STORE", default=None) is not None,
+            getenv("IN_NIX_SHELL", default=None) is not None,
+            str(which("python")).startswith(getenv("NIX_STORE", default="")),
+        ]
+    ):
+        print(
+            "tomlantic tests: note: nix detected, using whichever python is executing this",
+            " ... if you would like to test across multiple versions, run `nix run .#tests`",
+            sep="\n",
+            file=stderr,
+        )
+        versions_and_binaries["local"] = [executable]
+
+    elif bin_uv := which("uv"):
+        print("tomlantic tests: note: using uv for python management", file=stderr)
+        versions_and_binaries["39"] = [bin_uv, "run", "--python", "python3.9"]
+        versions_and_binaries["310"] = [bin_uv, "run", "--python", "python3.10"]
+        versions_and_binaries["311"] = [bin_uv, "run", "--python", "python3.11"]
+        versions_and_binaries["312"] = [bin_uv, "run", "--python", "python3.12"]
+        versions_and_binaries["313"] = [bin_uv, "run", "--python", "python3.13"]
+        versions_and_binaries["314"] = [bin_uv, "run", "--python", "python3.14"]
+
+    elif len(argv) > 1:
+        print(
+            "tomlantic tests: note: using argument-provided python binaries",
+            file=stderr,
+        )
+        # the argv is passed in from the nix flake like a key value pair
+        # (a la `.../python tests/test.py -- 3.9=/nix/store/... 3.10=/nix/store/... ...`)
+        for arg in argv[1:]:
+            version, binary_path = arg.split("=")
+            if version in versions_and_binaries:
+                versions_and_binaries[version].append(binary_path)
+
+    else:
+        print(
+            "tomlantic tests: note: using whichever python is executing this",
+            file=stderr,
+        )
+        versions_and_binaries["local"] = [executable]
+
+    for version, binary in versions_and_binaries.items():
+        print(
+            f" ... {version} -> {'`' + ' '.join(binary) + ' ... `' if binary else '(unresolved)'}",
+            file=stderr,
+        )
+
+    return versions_and_binaries
+
+
+def main() -> None:
+    versions_and_binaries = resolve_binaries()
     failed_tests: dict[str, str] = {}
     total_tests = 0
 
-    for test_path in Path(__file__).parent.glob("test-*.py"):
-        cp: CompletedProcess[bytes] = run(
-            f"uv run {test_path}", shell=True, capture_output=True
-        )
-        total_tests += 1
-
-        if cp.returncode == 0:
+    for version, binary in versions_and_binaries.items():
+        if len(binary) == 0:  # unresolved or not given
             continue
-        failed_tests[test_path.name] = cp.stderr.decode("utf-8")
+
+        for test_path in Path(__file__).parent.glob("test-*.py"):
+            print(
+                f"tomlantic tests: running {test_path.name}@py{version}...", file=stderr
+            )
+            cp: CompletedProcess[bytes] = run(
+                [*binary, test_path],
+                capture_output=True,
+            )
+            total_tests += 1
+
+            if cp.returncode == 0:
+                continue
+
+            failed_tests[f"{test_path.name}@py{version}"] = cp.stderr.decode("utf-8")
 
     if len(failed_tests) == 0:
         print(
@@ -24,6 +100,7 @@ def main():
             file=stderr,
         )
         exit(0)
+
     else:
         print(
             f"tomlantic tests: failed {len(failed_tests)}/{total_tests} ({len(failed_tests) / total_tests * 100:.2f}%) tests"
